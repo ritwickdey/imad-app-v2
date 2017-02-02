55d0b5061b8d7169871b7d80fc5c37cfff6b5eae
Ritwick Dey <dey9733@hotmail.com>
2017-02-03 00:17:58 +0530

    My Profile added

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter = 0;
+app.get('/friend/counter',function(req, res){
+  counter++;
+  res.send(counter.toString());
+});
+
+app.get('/friend/getCounter',function(req, res){
+  res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,40 @@
-console.log('Loaded!');
+document.getElementById("addFriendBtn").onclick = function(){
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE ){
+            if(request.status == 200){
+               var counter=  request.responseText;
+                document.getElementById("totalFriendCount").innerText = counter;
+                document.getElementById("addFriendBtn").innerText = "Friend request sent";
+                document.getElementById("addFriendBtn").disabled = true; 
+            }
+        }
+
+    };
+
+    request.open("GET","/friend/counter",true);
+    request.send(null);
+};
+
+window.onload = function(){
+ var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE ){
+            if(request.status == 200){
+               var counter= request.responseText;
+                document.getElementById("totalFriendCount").innerText = counter;
+            }
+            
+        }
+       
+
+    };
+
+    request.open("GET","/friend/getCounter",true);
+    request.send(null);
+}   
+
+
